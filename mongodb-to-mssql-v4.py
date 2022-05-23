6c4bb7b9e1c1eaf11d03d2e990220ec548d7d428
AhmedArshed <p176099@nu.edu.pk>
2022-05-23 13:15:06 +0500

    update

--- mongodb-to-mssql-v4.py
+++ mongodb-to-mssql-v4.py
@@ -9,82 +9,55 @@ import uuid
 import concurrent.futures
 from datetime import datetime
 from threading import current_thread
-
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 mongodb = client["utilities-scraper"]
-
 user = 'api'
 password = 'm7boukyvT.TedQ'
 database = 'amplify'
 server = 'electrical.database.windows.net'
 
 pyodbc.pooling = False
-
-connection_str = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={user};PWD={password};MultipleActiveResultSets=True "
-
+connection_str = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={user};PWD={password};MultipleActiveResultSets=True; "
 COMPETITORS = [
     "alliedelec", "baypower", "breakerauthority", "breakerhunter", "breakeroutlet", "chartercontact", "circuitbreaker", "coasttocoastbreaker", "controlparts",
     "dkhardware", "galco", "gordonelectricsupply", "imc_direct", "radwell", "relectric", "southlandelectrical", "superbreakers", "swgr", "thermaloverloadheatersunits", "zoro",
 ]
-
 DOCS_TO_SKIP = 0
-
 DOCS_CHUNK_SIZE = 10000
-
 SELECT_PRODUCTS = "SELECT id, competitor, their_name FROM scraper_competitor_products_new WHERE competitor IN ({0}) AND their_name IN ({1});"
-
 INSERT_PRODUCT = "INSERT INTO scraper_competitor_products_new (id, url, alternate_names, captured_at, competitor, cutsheet_url, description, long_description, manufacturers, their_name, created_at, updated_at, extracted_name) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, ?);"
-
-UPDATE_PRODUCT = "UPDATE scraper_competitor_products_new SET url = ?, alternate_names = ?, competitor = ?, cutsheet_url = ?, description = ?, long_description = ?, manufacturers = ?, their_name = ?, updated_at = CURRENT_TIMESTAMP, extracted_name = ?\
+UPDATE_PRODUCT = "UPDATE scraper_competitor_products_new WITH (ROWLOCK) SET url = ?, alternate_names = ?, competitor = ?, cutsheet_url = ?, description = ?, long_description = ?, manufacturers = ?, their_name = ?, updated_at = CURRENT_TIMESTAMP, extracted_name = ?\
     WHERE competitor = ? AND their_name = ?;"
-
 SELECT_IMAGES = "SELECT * FROM scraper_competitor_product_images_new WHERE scraper_competitor_product_id in (?);"
-
 INSERT_IMAGE = "INSERT INTO scraper_competitor_product_images_new (id, scraper_competitor_product_id, src, alt, title, created_at, updated_at) VALUES (NEWID(), ?, ?, ?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);"
-
-UPDATE_IMAGE = "UPDATE scraper_competitor_product_images_new SET src = ?, alt = ?, title = ?, updated_at = CURRENT_TIMESTAMP\
+UPDATE_IMAGE = "UPDATE scraper_competitor_product_images_new WITH (ROWLOCK) SET src = ?, alt = ?, title = ?, updated_at = CURRENT_TIMESTAMP\
     WHERE scraper_competitor_product_id = ?;"
-
 SELECT_IMAGES = "SELECT * FROM scraper_competitor_product_offers_new WHERE scraper_competitor_product_id in (?);"
-
 INSERT_OFFER = "INSERT INTO scraper_competitor_product_offers_new (id, scraper_competitor_product_id, condition_id, created_at, updated_at, stock, price, currency) VALUES (NEWID(), ?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, ?, ?, ?);"
-
-UPDATE_OFFER = "UPDATE scraper_competitor_product_offers_new SET condition_id = ?, updated_at = CURRENT_TIMESTAMP, stock = ?, price = ?, currency = ?\
+UPDATE_OFFER = "UPDATE scraper_competitor_product_offers_new WITH (ROWLOCK) SET condition_id = ?, updated_at = CURRENT_TIMESTAMP, stock = ?, price = ?, currency = ?\
     WHERE scraper_competitor_product_id = ?;"
-
-
 def get_new_cursor():
     conn = pyodbc.connect(connection_str)
     conn.autocommit = True
     return conn.cursor()
-
-
 def get_docs_to_skip():
     return DOCS_TO_SKIP
-
-
 def set_docs_to_skip(count):
     global DOCS_TO_SKIP
     DOCS_TO_SKIP += count
     with open("docs_to_skip.jl", "w+") as file:
         json.dump({"docs_to_skip": DOCS_TO_SKIP}, file)
-
-
 def get_docs_to_skip_from_json_file():
     if os.path.exists("./docs_to_skip.jl"):
         with open("docs_to_skip.jl") as file:
             for line in file:
                 result = json.loads(line)
                 set_docs_to_skip(result["docs_to_skip"])
-
-
 def send_slack_message(message):
-    payload = '{"text":"%s"}' % message
-    response = requests.post('https://hooks.slack.com/services/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'
-    , data = payload)
+    payload = json.dumps({"text": message.replace('"', "'")})
+    response = requests.post(
+        'https://hooks.slack.com/services/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', data=payload)
     return response
-
-
 def list_to_csv_string(li):
     if li is None or type(li) is str:
         return li
@@ -94,13 +67,9 @@ def list_to_csv_string(li):
             csv_string += f",{item}" if len(
                 csv_string) > 0 else f"{item}"
         return csv_string
-
-
 def split_in_chunks(a, n):
     k, m = divmod(len(a), n)
     return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
-
-
 def get_products_chunk_from_mongo(data_size=DOCS_CHUNK_SIZE):
     try:
         docs_to_skip = get_docs_to_skip()
@@ -111,8 +80,6 @@ def get_products_chunk_from_mongo(data_size=DOCS_CHUNK_SIZE):
         message = "Error: " + str(e) + "\n" + traceback.format_exc()
         send_slack_message(message)
         print(message)
-
-
 def get_products_from_mssql(cursor, products):
     try:
         competitors = tuple(pydash.uniq(pydash.map_(products, "competitor")))
@@ -124,8 +91,6 @@ def get_products_from_mssql(cursor, products):
         message = "Error: " + str(e) + "\n" + traceback.format_exc()
         send_slack_message(message)
         print(message)
-
-
 def get_condition_id(condition):
     if condition in ["newInBox", "newIndBox"]:
         return "be900e40-8041-464b-951e-c15009df8132"
@@ -137,8 +102,6 @@ def get_condition_id(condition):
         return "5ce27e59-c7ca-4baa-9368-15dac5d3b180"
     else:
         return condition
-
-
 def upsert_products(cursor, products_to_insert, products_to_update):
     try:
         if len(products_to_insert) > 0:
@@ -149,8 +112,6 @@ def upsert_products(cursor, products_to_insert, products_to_update):
         message = "Error: " + str(e) + "\n" + traceback.format_exc()
         send_slack_message(message)
         print(message)
-
-
 def upsert_images(cursor, images_to_insert, images_to_update):
     try:
         if len(images_to_insert) > 0:
@@ -161,8 +122,6 @@ def upsert_images(cursor, images_to_insert, images_to_update):
         message = "Error: " + str(e) + "\n" + traceback.format_exc()
         send_slack_message(message)
         print(message)
-
-
 def upsert_offers(cursor, offers_to_insert, offers_to_update):
     try:
         if len(offers_to_insert) > 0:
@@ -173,8 +132,6 @@ def upsert_offers(cursor, offers_to_insert, offers_to_update):
         message = "Error: " + str(e) + "\n" + traceback.format_exc()
         send_slack_message(message)
         print(message)
-
-
 def process_data(db_results, data):
     products_to_insert = []
     products_to_update = []
@@ -239,8 +196,6 @@ def process_data(db_results, data):
         "images_to_insert": images_to_insert, "images_to_update": images_to_update,
         "offers_to_insert": offers_to_insert, "offers_to_update": offers_to_update
     }
-
-
 def save_data_to_mssql(products):
     try:
         cursor = current_thread().cursor
@@ -252,31 +207,25 @@ def save_data_to_mssql(products):
                       data["images_to_update"])
         upsert_offers(cursor, data["offers_to_insert"],
                       data["offers_to_update"])
-        set_docs_to_skip(DOCS_TO_SKIP)
     except Exception as e:
         message = "Error: " + str(e) + "\n" + traceback.format_exc()
         send_slack_message(message)
         print(message)
-
-
 def initializer_worker():
     current_thread().cursor = get_new_cursor()
-
-
 def main():
     get_docs_to_skip_from_json_file()
     executor = concurrent.futures.ThreadPoolExecutor(
-        2, initializer=initializer_worker)
+        20, initializer=initializer_worker)
     products = get_products_chunk_from_mongo()
     while len(products) > 0:
         products_chunks = split_in_chunks(products, 20)
         futures = [executor.submit(save_data_to_mssql, chunk) for chunk in products_chunks]
         concurrent.futures.wait(futures)
+        set_docs_to_skip(DOCS_CHUNK_SIZE)
         if len(products) < DOCS_CHUNK_SIZE:
             break
-        # products = get_products_chunk_from_mongo()
+        products = get_products_chunk_from_mongo()
     set_docs_to_skip(0)
-
-
 if __name__ == '__main__':
     main()
